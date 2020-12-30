2d01a589e0d205fc498a778ae3f2f4eb14e6202d
nnewsom99 <48023578+nnewsom99@users.noreply.github.com>
2020-12-30 17:00:56 -0500

    Initial commit.

---
+++ script.js
@@ -0,0 +1,8 @@
+/*import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<p>loremipsumlorenipsum</p>, document.getElementById("root"));*/
+
+function sampleText( id){
+    document.getElementById(id).innerHTML = "<p>loremipsumlorenipsum</p>";
+}
